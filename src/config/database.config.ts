ea6c04ecfd20adae26eab73cdc7dd6920c0e5097
kong <kong.hou@nine.com.au>
2022-04-28 22:23:11 +1000

    feat(wip): use configuration

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,7 +1,9 @@
-export default () => ({
-  postgresHost: process.env.DB_POSTGRES_HOST,
-  postgresPort: process.env.DB_POSTGRES_PORT,
-  postgresUsername: process.env.DB_POSTGRES_USERNAME,
-  postgresPassword: process.env.DB_POSTGRES_PASSWORD,
-  postgresDatabaseName: process.env.DB_POSTGRES_DATABASE_NAME,
-});
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('postgres', () => ({
+  host: process.env.DB_POSTGRES_HOST,
+  port: process.env.DB_POSTGRES_PORT,
+  username: process.env.DB_POSTGRES_USERNAME,
+  password: process.env.DB_POSTGRES_PASSWORD,
+  databaseName: process.env.DB_POSTGRES_DATABASE_NAME,
+}));
